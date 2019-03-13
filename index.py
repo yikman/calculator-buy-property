41f89a25206a382b6f4f85e49fb2c4b1df9dea6d
Isaac Shek <isaac.shek@amido.com>
2019-03-13 12:20:44 +0000

    updating help to buy calculation deposit

--- index.py
+++ index.py
@@ -1,25 +1,26 @@
-propertyPrice = float(input("What is the property price? ") )
+propertyPrice = int(input("What is the property price? ") )
 
 print('----------------------')
 while True:
   isHelpToBuy = input("Does it include help to buy scheme? 1 - Yes | 2 - No: ")
   if isHelpToBuy == 1:
     helpToBuyPrice = (propertyPrice * 0.2)
+    depositAmount = (propertyPrice * 0.05)
     print('Include help to buy scheme')
     break
   elif isHelpToBuy == 2:
     helpToBuyPrice = 0
     print('Exclude help to buy scheme')
+    depositAmount = int (input("How much are you putting in deposit? "))
     break
   else:
     print("Please enter y or n.")
 
 print('----------------------')
-depositAmount = float (input("How much are you putting in deposit? "))
-
-print('Property Price:', propertyPrice)
-print('Equity loan:', helpToBuyPrice)
-print('Deposit: ', depositAmount)
 
 mortgageRequired = propertyPrice - helpToBuyPrice - depositAmount
-print('Mortgage amount required:', mortgageRequired)
+
+print('Property Price: ', propertyPrice)
+print('Equity loan: ', helpToBuyPrice)
+print('Deposit: ', depositAmount)
+print('Mortgage amount required: ', mortgageRequired)
